2b5a47b9b05eee0f0499ca9391fdfdd8e15d184d
Beshoy Gamal <Beshoy.Gamal@itworx.com>
2020-02-18 07:47:45 +0200

    upload theme

---
+++ src/context/products.js
@@ -0,0 +1,37 @@
+// products context
+import React,{useEffect} from 'react'
+import axios from 'axios';
+import url from '../utils/URL';
+import { featuredProducts } from './../utils/helpers';
+export const ProductContext = React.createContext();
+
+
+//Provider , Consumer , useContext()
+export default function ProductProvider({children}) {
+   
+    const [loading,setLoading] = React.useState(false);
+    const [products,setProducts] = React.useState([]);
+    const [featured,setFeatured] = React.useState([]);
+
+    useEffect(() => {
+        setLoading(true);
+        axios.get(`${url}/products`).then(response => {
+
+            const featured = featuredProducts(response.data);
+            setFeatured(featured);
+            setProducts(response.data);
+            setLoading(false);
+        });
+      
+           return ()=>{
+       
+        }
+       }, [])
+
+    return (
+        <ProductContext.Provider value={{products,loading,featured}}>
+           {children} 
+        </ProductContext.Provider>
+    )
+}
+
---
+++ src/pages/About.js
@@ -0,0 +1,18 @@
+import React from "react";
+
+export default function About() {
+  return (
+    <section className="section about-section">
+      <h1>About us</h1>
+      <p>
+        Lorem ipsum dolor amet flannel succulents live-edge biodiesel bespoke,
+        90's kombucha helvetica ramps irony iPhone. Subway tile tote bag
+        waistcoat keytar poke. Brunch pickled art party lyft, vinyl leggings
+        vape chia direct trade. Pabst tumeric pok pok irony next level. Craft
+        beer enamel pin direct trade locavore sartorial, cardigan scenester
+        drinking vinegar fingerstache cornhole vaporware snackwave sustainable
+        lomo bitters.
+      </p>
+    </section>
+  );
+}
